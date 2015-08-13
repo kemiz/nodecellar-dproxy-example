f0f5ccdc5c5d01ef0f575b16a5929c0dc3f31143
kemiz <kemimeister@gmail.com>
2015-08-13 12:26:51 +0100

    bump to 3.2

--- nodejs/scripts/mongo/set-mongo-url.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-ctx source instance runtime_properties mongo_ip_address $(ctx target instance runtime_properties outputs.endpoint.value.ip_address)
-ctx source instance runtime_properties mongo_port $(ctx target instance runtime_properties outputs.endpoint.value.port)
-
